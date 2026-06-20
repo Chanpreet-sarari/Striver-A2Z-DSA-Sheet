8cfc210b970f6e8a3b48366f8a036de7a7dea3f5
Chanpreet Sarari <chanpreetsarari5422@gmail.com>
2026-06-20 08:56:30 +0530

    change in code

--- Math/Palindrome_or_Not.cpp
+++ Math/Palindrome_or_Not.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 // first method tc: O(log10 (n)) sc: O(1)
-// int reverse_number(int n)
-// {
-//     int rev =  0 ;
-//     while(n > 0)
-//     {
-//         int lastdigit = n %10 ;
-//         rev = rev *10 + lastdigit;
-//         n = n / 10 ;
-//     }
-//     return rev;
-// }
+int reverse_number(int n)
+{
+    int rev =  0 ;
+    while(n > 0)
+    {
+        int lastdigit = n %10 ;
+        rev = rev *10 + lastdigit;
+        n = n / 10 ;
+    }
+    return rev;
+}
 
 
 int main()
